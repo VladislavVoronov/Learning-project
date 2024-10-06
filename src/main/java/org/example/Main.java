0f1472237c195263dae013c44d8ef00f3d51e600
axwell-wq <Stepan07mail@gmail.com>
2024-10-06 15:13:45 +0300

    Выполнил задачу Сумма массива

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
+// Сумма массива: Напишите метод, который принимает массив целых чисел и возвращает их сумму.
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = new int[3];
+        Scanner console = new Scanner(System.in);
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = console.nextInt();
+        }
+
+        System.out.println(getSum(array) + 10);
+    }
+
+    public static int getSum(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+
+        return sum;
     }
 }
